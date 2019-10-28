ee7b3e47d2df0d2b3c7736741bae74afcd674f3a
Brant <xkdcc@users.noreply.github.com>
2019-10-28 13:22:25 -0700

    Integrate pytest with setup.py.

--- setup.py
+++ setup.py
@@ -1,4 +1,31 @@
 import setuptools
+import sys
+
+# import pdb
+
+from setuptools.command.test import test as TestCommand
+
+# pdb.set_trace()
+
+class PyTest(TestCommand):
+    description = "run pytest after in-place build"
+    user_options = [
+        ('pytest-args=', 'a', 'Arguments to pass to pytest')
+        ]
+  
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+    
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        
+        errno = pytest.main(self.pytest_args.split())
+        sys.exit(errno)
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
@@ -23,4 +50,7 @@ setuptools.setup(
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
+    
+     tests_require=['pytest'], # With tests_require, setup.py will install the packages if not installed yet.
+     cmdclass = {'test': PyTest}, # This is to add "test" as our customized step into setup.py.
  )
